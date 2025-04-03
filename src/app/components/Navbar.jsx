29ac82c5452b4e015ca7b0749ca0dc18822d037b
chandan <chandansamantaray784@gmail.com>
2025-04-03 20:18:22 +0530

    login change

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -49,7 +49,7 @@ export default function Navbar() {
                 onClick={handleLogin}
                 className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition duration-300"
               >
-                Login
+                Log
               </button>
             )}
           </div>
